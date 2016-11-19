c6f55aed176c26e610e274027ddfdf1d2e56cd78
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-11-18 21:13:57 -0800

    add events

---
+++ src/components/events/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="events">
+        <h2>Events</h2>
+      </div>
+    )
+  }
+}
+
+export default Footer
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './app'
 import Home from './components/home'
+import Events from './components/events'
 import './style/app.css';
 
 const Routes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
+      <Route path="/events" component={Events} />
       <Route path="*" component={Home} />
     </Route>
   </Router>
