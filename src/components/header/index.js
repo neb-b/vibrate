7732c276f56910656710bb8944f5bc308eda4beb
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-11-18 21:20:03 -0800

    header link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 import '../../style/header.css'
 
 class Header extends Component {
   render() {
     return (
-      <div className="header"></div>
+      <div className="header">
+        <Link to="/">
+          <h1 className="title">Home</h1>
+        </Link>
+      </div>
     )
   }
 }
