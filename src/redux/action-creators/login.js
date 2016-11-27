0a70c4092640a177675f854a2e0115138a5643a5
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-11-26 19:25:30 -0800

    fix dispatch problem

--- src/redux/action-creators/login.js
+++ src/redux/action-creators/login.js
@@ -6,17 +6,15 @@ import {
   LOGIN_SUCCESS
 } from '../constants/constants'
 
-const login = () => {
-  console.log("start login")
-  return function (dispatch) {
-    dispatch(createAction(LOGIN_REQUEST))
-    return postJson('/login')
-      .then(res => res.json())
-      .then(data =>
-        dispatch(createAction(LOGIN_SUCCESS, { data }))
-      )
-      .catch(error => dispatch(createAction(LOGIN_ERROR, { error })))
-  }
+const login = (values, dispatch) => {
+  const payload = {...values}
+  dispatch(createAction(LOGIN_REQUEST))
+  return postJson('/login', payload)
+    .then(res => res.json())
+    .then(data =>
+      dispatch(createAction(LOGIN_SUCCESS, { data }))
+    )
+    .catch(error => dispatch(createAction(LOGIN_ERROR, { error })))
 }
 
 export default login
